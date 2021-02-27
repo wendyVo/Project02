b024c5ad8ab74c2118bc0f612f994ced2308dcda
pfotis <69961508+pfotis@users.noreply.github.com>
2021-02-28 09:14:08 +1100

    create the colums for the menu table

--- models/menu.js
+++ models/menu.js
@@ -1,4 +1,4 @@
-module.exports = (sequelize, DataTypes) => {
+odule.exports = (sequelize, DataTypes) => {
     const Menu = sequelize.define('Menu', {
       name: {
         type: DataTypes.TEXT,
@@ -8,23 +8,5 @@ module.exports = (sequelize, DataTypes) => {
         }
       }
     });
-    //Menu table with  Ingredient Table connect "many to many"
-    Menu.associate = function(models) {
-        Menu.belongsToMany(models.Ingredient, {
-          through: 'IngredientMenus',
-          as: 'ingredients',
-          foreignKey: 'menuId',
-          otherKey: 'ingredientId'
-        });
-    };
-    //Menu table with Table Table connect "many to many"
-    Menu.associate = function(models) {
-        Menu.belongsToMany(models.Table, {
-          through: 'TableMenus',
-          as: 'tables',
-          foreignKey: 'menuId',
-          otherKey: 'tableId'
-        });
-      };
     return Menu;
 };
