42094399295588c18c15f590118fc49029a0dd08
pfotis <69961508+pfotis@users.noreply.github.com>
2021-03-02 20:59:41 +1100

    create app.get from the table employee

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,1 +1,19 @@
+const pinNumber = 1234;
 
+const getPinNumber = () => {
+  // console.log('Get employee is getting called');
+  fetch(`/api/employee/${pinNumber}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      window.location.replace(`/${data.position}`);
+    })
+    .catch(error => console.error("Error:", error));
+};
+
+// Get the link of the employee
+getPinNumber();
