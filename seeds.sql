4a051e396fa9b267c138abab3b5affb42d5a0222
avpizarro <avpizarro@live.com>
2021-03-05 14:56:46 +1100

    Update to waiter.hdb to prepare for js

---
+++ seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO restaurant_db.employees (firstName, lastName, pinNumber, position, managerId, createdAt, updatedAt) VALUES ("Jane","Doe", 1111, "waiter", 2, '9999-12-31 23:59:59', '9999-12-31 23:59:59');
+INSERT INTO restaurant_db.employees (firstName, lastName, pinNumber, position, managerId, createdAt, updatedAt) VALUES ("Yasmin","Khalil", 4444, "manager", 2, '9999-12-31 23:59:59', '9999-12-31 23:59:59');
+INSERT INTO restaurant_db.employees (firstName, lastName, pinNumber, position, managerId, createdAt, updatedAt) VALUES ("Wu","Xi", 2222, "waiter", 2, '9999-12-31 23:59:59', '9999-12-31 23:59:59');
+INSERT INTO restaurant_db.employees (firstName, lastName, pinNumber, position, managerId, createdAt, updatedAt) VALUES ("Christine","Lang", 3333, "chef", 2, '9999-12-31 23:59:59', '9999-12-31 23:59:59');
