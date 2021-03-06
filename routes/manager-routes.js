b71ec03a04723874e995ac3c0cc9d161acebf5a1
Wendy Uyen Vo <uyen199247@gmail.com>
2021-03-06 15:27:19 +1100

    added manager-routes.js, manager.handlebars and Manager page CSS

---
+++ routes/manager-routes.js
@@ -0,0 +1,31 @@
+const db = require('../models');
+
+module.exports = (app) => {
+    // Find all employees and return them to the user with res.json
+    app.get('/api/employees', (req, res) => {
+        db.Employee.findAll({
+
+        }).then((response) => res.json(response));
+    });
+
+    //Add new employee 
+    app.post('/api/employees', (req, res) => {
+        var { firstName, lastName, position, pinNumber, managerId } = req.body;
+
+        var newEmployee = {
+            firstName,
+            lastName,
+            position,
+            pinNumber,
+            managerId
+        }
+        console.log(newEmployee);
+        db.Employee.create(newEmployee).then((emp) => {
+
+            console.log(`Added employee ${emp.firstName} ${emp.lastName}`);
+            res.json({ id: emp.id });
+        });
+    });
+
+
+};
--- server.js
+++ server.js
@@ -21,14 +21,16 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 const routes = require("./routes/routes");
 const ingredientRoutes = require("./routes/ingredient-routes.js");
+const managerRoutes = require("./routes/manager-routes");
 
 ingredientRoutes(app);
+managerRoutes(app);
 routes(app);
 
 // Start our server so that it can begin listening to client requests.
 db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
+    app.listen(PORT, () => {
+        // Log (server-side) when our server has started
+        console.log("Server listening on: http://localhost:" + PORT);
+    });
 });
