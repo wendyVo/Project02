f629c303cdbacea8e3545624f473a1181b43bdcf
Wendy Uyen Vo <uyen199247@gmail.com>
2021-03-06 15:37:17 +1100

    fix lint check

--- routes/manager-routes.js
+++ routes/manager-routes.js
@@ -1,31 +1,26 @@
-const db = require('../models');
+const db = require("../models");
 
-module.exports = (app) => {
-    // Find all employees and return them to the user with res.json
-    app.get('/api/employees', (req, res) => {
-        db.Employee.findAll({
+module.exports = app => {
+  // Find all employees and return them to the user with res.json
+  app.get("/api/employees", (req, res) => {
+    db.Employee.findAll({}).then(response => res.json(response));
+  });
 
-        }).then((response) => res.json(response));
+  //Add new employee
+  app.post("/api/employees", (req, res) => {
+    const { firstName, lastName, position, pinNumber, managerId } = req.body;
+
+    const newEmployee = {
+      firstName,
+      lastName,
+      position,
+      pinNumber,
+      managerId
+    };
+    console.log(newEmployee);
+    db.Employee.create(newEmployee).then(emp => {
+      console.log(`Added employee ${emp.firstName} ${emp.lastName}`);
+      res.json({ id: emp.id });
     });
-
-    //Add new employee 
-    app.post('/api/employees', (req, res) => {
-        var { firstName, lastName, position, pinNumber, managerId } = req.body;
-
-        var newEmployee = {
-            firstName,
-            lastName,
-            position,
-            pinNumber,
-            managerId
-        }
-        console.log(newEmployee);
-        db.Employee.create(newEmployee).then((emp) => {
-
-            console.log(`Added employee ${emp.firstName} ${emp.lastName}`);
-            res.json({ id: emp.id });
-        });
-    });
-
-
+  });
 };
--- server.js
+++ server.js
@@ -29,8 +29,8 @@ routes(app);
 
 // Start our server so that it can begin listening to client requests.
 db.sequelize.sync().then(() => {
-    app.listen(PORT, () => {
-        // Log (server-side) when our server has started
-        console.log("Server listening on: http://localhost:" + PORT);
-    });
+  app.listen(PORT, () => {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
 });
