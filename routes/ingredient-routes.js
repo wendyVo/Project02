5a8ba146a97483a933c447fc66a9604c1fa856e3
pfotis <69961508+pfotis@users.noreply.github.com>
2021-03-08 08:09:22 +1100

    pass two query sequelize in the same page handlebar

--- routes/ingredient-routes.js
+++ routes/ingredient-routes.js
@@ -1,28 +1,30 @@
 const db = require("../models");
 const sequelize = require("sequelize");
 
+let inStock;
+let lowStock;
+
 module.exports = app => {
-  // get the ingredients where are greater minimun quantity
-  app.get("/ingredients", (req, res) => {
-    db.Ingredient.findAll({
-      where: {
-        quantity: { [sequelize.Op.gt]: sequelize.col("minimumQuantity") }
-      }
-    }).then(inStock => {
-      res.render("ingredients", { inStock });
-    });
+  // get the ingredients where are greater from the minimun quantity
+  db.Ingredient.findAll({
+    where: {
+      quantity: { [sequelize.Op.gt]: sequelize.col("minimumQuantity") }
+    }
+  }).then(data => {
+    inStock = data;
   });
+  // get the ingredients where are less or equal from the minimun quantity
+  db.Ingredient.findAll({
+    where: {
+      quantity: { [sequelize.Op.lte]: sequelize.col("minimumQuantity") }
+    }
+  }).then(data => {
+    lowStock = data;
+  });
+  console.log(`inStock: ${inStock} 
+  lowStock: ${lowStock}`);
 
-  // app.get("/ingredients", (req, res) => {
-  //   res.render("ingredients", { inStock: "data" });
-  // });
-
-  // get the ingredients where are less or equal minimun quantity
-  app.get("/api/ingredient", (req, res) => {
-    db.Ingredient.findAll({
-      where: {
-        quantity: { [sequelize.Op.lte]: sequelize.col("minimumQuantity") }
-      }
-    }).then(data => res.json(data));
+  app.get("/ingredients", (req, res) => {
+    res.render("ingredients", { inStock, lowStock });
   });
 };
