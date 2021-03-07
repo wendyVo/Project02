345340a90e22586fe2f5b6bd5f487a8b69ad8d67
avpizarro <avpizarro@live.com>
2021-03-07 21:02:09 +1100

    Make all button clikable

--- public/assets/js/waiters.js
+++ public/assets/js/waiters.js
@@ -2,22 +2,38 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM loaded! 🚀");
 
   // const menu = document.querySelector(".dish");
-  const tableBtn = document.querySelector(".tableBtn");
+  const tableBtn = document.querySelectorAll(".tableBtn");
+  const tableOrder = document.getElementById("tableOrder");
 
-  tableBtn.addEventListener("click", e => {
-    e.preventDefault();
-    console.log("clicked");
-    getDishes();
+  console.log(tableBtn);
+  tableBtn.forEach(button => {
+    button.addEventListener("click", e => {
+      e.preventDefault();
+      console.log("clicked");
+      hideShow();
+      // tableOrder.setAttribute("style", "display: none");
+      // getDishes();
+    });
   });
 
-  const getDishes = () => {
-    fetch("/api/dishes", {
-      method: "GET"
-      // headers: { "Content-Type:": "application/json" },
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-      });
+  const hideShow = () => {
+    if (tableOrder.className === "showing") {
+      tableOrder.className = "hidden";
+      tableOrder.setAttribute("style", "display: none");
+    } else if ((tableOrder.className = "hidden")) {
+      tableOrder.className = "showing";
+      tableOrder.setAttribute("style", "display: block");
+    }
   };
+
+  // const getDishes = () => {
+  //   fetch("/api/dishes", {
+  //     method: "GET",
+  //     // headers: { "Content-Type:": "application/json" },
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //     });
+  // };
 });
