9c2187b8298303271d31f8650d30ee9625c5eef3
pfotis <69961508+pfotis@users.noreply.github.com>
2021-03-07 11:20:24 +1100

    make autofill the id of all the tables

--- public/assets/js/ingredients.js
+++ public/assets/js/ingredients.js
@@ -1,39 +1,39 @@
-const getIngredientsGreater = () => {
-  console.log("Get ingredients greater minimumQuantity");
-  fetch("/api/ingredients", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      res.render("ingredients", data);
-      for (let i = 0; i < data.length; i++) {
-        console.log("consoloe log data" + data[i].name);
-      }
-    })
-    .catch(error => console.log(`error:, ${error}`));
-};
+// const getIngredientsGreater = () => {
+//   console.log("Get ingredients greater minimumQuantity");
+//   fetch("/api/ingredients", {
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json"
+//     }
+//   })
+//     .then(response => response.json())
+//     .then(data => {
+//       console.log(data);
+//       res.render("ingredients", data);
+//       for (let i = 0; i < data.length; i++) {
+//         console.log("consoloe log data" + data[i].name);
+//       }
+//     })
+//     .catch(error => console.log(`error:, ${error}`));
+// };
 
-const getIngredientsless = () => {
-  console.log("Get ingredients less  or equal minimumQuantity");
-  fetch("/api/ingredient", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      for (let i = 0; i < data.length; i++) {
-        console.log("consoloe log data" + data[i].name);
-      }
-    })
-    .catch(error => console.log(`error:, ${error}`));
-};
+// const getIngredientsless = () => {
+//   console.log("Get ingredients less  or equal minimumQuantity");
+//   fetch("/api/ingredient", {
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json"
+//     }
+//   })
+//     .then(response => response.json())
+//     .then(data => {
+//       console.log(data);
+//       for (let i = 0; i < data.length; i++) {
+//         console.log("consoloe log data" + data[i].name);
+//       }
+//     })
+//     .catch(error => console.log(`error:, ${error}`));
+// };
 
 // getIngredientsGreater();
 // getIngredientsless();
