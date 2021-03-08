1e205e753d1e9bd3ed5d29eeae5b45f14b576777
avpizarro <avpizarro@live.com>
2021-03-08 12:13:35 +1100

    Fix seeds.sql

--- seeds.sql
+++ seeds.sql
@@ -3,11 +3,11 @@ INSERT INTO restaurant_db.employees (firstName, lastName, pinNumber, position, m
 INSERT INTO restaurant_db.employees (firstName, lastName, pinNumber, position, managerId, createdAt, updatedAt) VALUES ("Wu","Xi", 2222, "waiter", 2, '9999-12-31 23:59:59', '9999-12-31 23:59:59');
 INSERT INTO restaurant_db.employees (firstName, lastName, pinNumber, position, managerId, createdAt, updatedAt) VALUES ("Christine","Lang", 3333, "chef", 2, '9999-12-31 23:59:59', '9999-12-31 23:59:59');
 
-INSERT INTO restaurant_db.RestaurantTables (numCustomers, isavailable, dimension, createdAt, updatedAt) VALUES (4, true,"50x50", '9999-12-31 23:59:59', '9999-12-31 23:59:59');
-INSERT INTO restaurant_db.RestaurantTables (numCustomers, isavailable, dimension, createdAt, updatedAt) VALUES (4, true,"50x50", '9999-12-31 23:59:59', '9999-12-31 23:59:59');
-INSERT INTO restaurant_db.RestaurantTables (numCustomers, isavailable, dimension, createdAt, updatedAt) VALUES (4, true,"50x50", '9999-12-31 23:59:59', '9999-12-31 23:59:59');
-INSERT INTO restaurant_db.RestaurantTables (numCustomers, isavailable, dimension, createdAt, updatedAt) VALUES (6, true,"80x50", '9999-12-31 23:59:59', '9999-12-31 23:59:59');
-INSERT INTO restaurant_db.RestaurantTables (numCustomers, isavailable, dimension, createdAt, updatedAt) VALUES (6, true,"50x50", '9999-12-31 23:59:59', '9999-12-31 23:59:59');
+INSERT INTO restaurant_db.RestaurantTables (numCustomers, isavailable, dimension, createdAt, updatedAt) VALUES (4, true, "100x100", '9999-12-31 23:59:59', '9999-12-31 23:59:59');
+INSERT INTO restaurant_db.RestaurantTables (numCustomers, isavailable, dimension, createdAt, updatedAt) VALUES (4, true, "100x100", '9999-12-31 23:59:59', '9999-12-31 23:59:59');
+INSERT INTO restaurant_db.RestaurantTables (numCustomers, isavailable, dimension, createdAt, updatedAt) VALUES (4, true, "100x100", '9999-12-31 23:59:59', '9999-12-31 23:59:59');
+INSERT INTO restaurant_db.RestaurantTables (numCustomers, isavailable, dimension, createdAt, updatedAt) VALUES (6, true,"150x100", '9999-12-31 23:59:59', '9999-12-31 23:59:59');
+INSERT INTO restaurant_db.RestaurantTables (numCustomers, isavailable, dimension, createdAt, updatedAt) VALUES (6, true,"150x100", '9999-12-31 23:59:59', '9999-12-31 23:59:59');
 
 INSERT INTO restaurant_db.Dishes (title, price, isReady, createdAt, updatedAt) VALUES ("SESAME PEANUT SICHUAN NOODLE", 16, 0, '9999-12-31 23:59:59', '9999-12-31 23:59:59');
 INSERT INTO restaurant_db.Dishes (title, price, isReady, createdAt, updatedAt) VALUES ("TURMERIC FRIED CHICKEN", 16, 0, '9999-12-31 23:59:59', '9999-12-31 23:59:59');
