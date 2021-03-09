1d4ecd3d54509bd8b0f5e38f77f69bcf2650d2b0
Wendy Uyen Vo <uyen199247@gmail.com>
2021-03-09 22:27:23 +1100

    added delete employee functionality

--- public/assets/js/manager.js
+++ public/assets/js/manager.js
@@ -1,75 +1,76 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("DOM loaded! 🚀");
+  console.log("DOM loaded! 🚀");
 
-    // delete Employee
-    const deleteEmp = document.querySelectorAll(".deleteEmp");
-    console.log(deleteEmp);
-    if (deleteEmp) {
-        deleteEmp.forEach((emp) => {
-            emp.addEventListener("click", (e) => {
-                e.preventDefault();
-                console.log("clicked");
-                const id = emp.getAttribute("data-id");
-                console.log(id);
+  // delete Employee
+  const deleteEmp = document.querySelectorAll(".deleteEmp");
+  console.log(deleteEmp);
+  if (deleteEmp) {
+    deleteEmp.forEach(emp => {
+      emp.addEventListener("click", e => {
+        e.preventDefault();
+        console.log("clicked");
+        const id = emp.getAttribute("data-id");
+        console.log(id);
 
-                fetch(`/api/employees/${id}`, {
-                    method: 'DELETE',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                }).then((response) => {
-                    alert(`${response.firstName} ${response.lastName} removed employee successly`);
-                    window.location.replace("/manager");
-                });
-            });
+        fetch(`/api/employees/${id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }).then(response => {
+          alert(
+            `${response.firstName} ${response.lastName} removed employee successly`
+          );
+          window.location.replace("/manager");
         });
-    };
+      });
+    });
+  }
 
-    //add new employee functionality
-    const addEmpBtn = document.getElementById("addEmpBtn");
+  //add new employee functionality
+  const addEmpBtn = document.getElementById("addEmpBtn");
 
-    if (addEmpBtn) {
-        addEmpBtn.addEventListener("click", e => {
-            e.preventDefault();
-            console.log(addEmpBtn);
-            alert("btn clicked")
-            const newEmployee = {
-                firstName: $("#firstName")
-                    .val()
-                    .trim(),
-                lastName: $("#lastName")
-                    .val()
-                    .trim(),
-                email: $("#email")
-                    .val()
-                    .trim(),
-                phone: $("#phone")
-                    .val()
-                    .trim(),
-                pinNumber: $("#pinNumber")
-                    .val()
-                    .trim(),
-                position: $("#position")
-                    .val()
-                    .trim(),
-                managerId: $("#managerId")
-                    .val()
-                    .trim()
-            };
-            console.log("this is " + newEmployee);
-            fetch("/api/employees", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify(newEmployee)
-                })
-                .then(() => {
-                    alert("Added employee successly");
-                    window.location.replace("/manager");
-                })
-                .catch(err => console.error(err));
-        });
-    };
-
+  if (addEmpBtn) {
+    addEmpBtn.addEventListener("click", e => {
+      e.preventDefault();
+      console.log(addEmpBtn);
+      alert("btn clicked");
+      const newEmployee = {
+        firstName: $("#firstName")
+          .val()
+          .trim(),
+        lastName: $("#lastName")
+          .val()
+          .trim(),
+        email: $("#email")
+          .val()
+          .trim(),
+        phone: $("#phone")
+          .val()
+          .trim(),
+        pinNumber: $("#pinNumber")
+          .val()
+          .trim(),
+        position: $("#position")
+          .val()
+          .trim(),
+        managerId: $("#managerId")
+          .val()
+          .trim()
+      };
+      console.log("this is " + newEmployee);
+      fetch("/api/employees", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newEmployee)
+      })
+        .then(() => {
+          alert("Added employee successly");
+          window.location.replace("/manager");
+        })
+        .catch(err => console.error(err));
+    });
+  }
 });
