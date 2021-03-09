00f7f314844940fe1680a13fe5570d050ce90b68
Wendy Uyen Vo <uyen199247@gmail.com>
2021-03-09 22:23:28 +1100

    added delete employee functionality

--- public/assets/js/manager.js
+++ public/assets/js/manager.js
@@ -1,62 +1,75 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM loaded! 🚀");
-  //add new employee functionality
-  const addEmpBtn = document.getElementById("addEmpBtn");
-  addEmpBtn.addEventListener("click", e => {
-    e.preventDefault();
+    console.log("DOM loaded! 🚀");
 
-    const newEmployee = {
-      firstName: $("#firstName")
-        .val()
-        .trim(),
-      lastName: $("#lastName")
-        .val()
-        .trim(),
-      email: $("#email")
-        .val()
-        .trim(),
-      phone: $("#phone")
-        .val()
-        .trim(),
-      pinNumber: $("#pinNumber")
-        .val()
-        .trim(),
-      position: $("#position")
-        .val()
-        .trim(),
-      managerId: $("#managerId")
-        .val()
-        .trim()
+    // delete Employee
+    const deleteEmp = document.querySelectorAll(".deleteEmp");
+    console.log(deleteEmp);
+    if (deleteEmp) {
+        deleteEmp.forEach((emp) => {
+            emp.addEventListener("click", (e) => {
+                e.preventDefault();
+                console.log("clicked");
+                const id = emp.getAttribute("data-id");
+                console.log(id);
+
+                fetch(`/api/employees/${id}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }).then((response) => {
+                    alert(`${response.firstName} ${response.lastName} removed employee successly`);
+                    window.location.replace("/manager");
+                });
+            });
+        });
     };
-    fetch("/api/employees", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(newEmployee)
-    })
-      .then(() => {
-        alert("Added employee successly");
-        window.location.replace("/manager");
-      })
-      .catch(err => console.error(err));
-  });
 
-  //delete Employee Functionality
-  const deleteEmp = document.getElementById("deleteEmp");
+    //add new employee functionality
+    const addEmpBtn = document.getElementById("addEmpBtn");
 
-  deleteEmp.addEventListener("click", e => {
-    e.preventDefault();
-    alert("clicked");
-    const id = e.target.parentElement("tr").getAttribute("data-id");
-    console.log(id);
+    if (addEmpBtn) {
+        addEmpBtn.addEventListener("click", e => {
+            e.preventDefault();
+            console.log(addEmpBtn);
+            alert("btn clicked")
+            const newEmployee = {
+                firstName: $("#firstName")
+                    .val()
+                    .trim(),
+                lastName: $("#lastName")
+                    .val()
+                    .trim(),
+                email: $("#email")
+                    .val()
+                    .trim(),
+                phone: $("#phone")
+                    .val()
+                    .trim(),
+                pinNumber: $("#pinNumber")
+                    .val()
+                    .trim(),
+                position: $("#position")
+                    .val()
+                    .trim(),
+                managerId: $("#managerId")
+                    .val()
+                    .trim()
+            };
+            console.log("this is " + newEmployee);
+            fetch("/api/employees", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(newEmployee)
+                })
+                .then(() => {
+                    alert("Added employee successly");
+                    window.location.replace("/manager");
+                })
+                .catch(err => console.error(err));
+        });
+    };
 
-    $.ajax({
-      method: "DELETE",
-      url: `/api/employees/${id}`
-    }).then(() => {
-      alert("Removed employee successly");
-      window.location.replace("/manager");
-    });
-  });
 });
